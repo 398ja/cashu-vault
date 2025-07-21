ee23928a9046a7e893a8fe4a2f3e294bb909b078
Eric T <tchepane@hey.com>
2025-07-21 20:37:12 +0100

    Add Dockerfile and fix newlines

--- cashu-vault-jpa/src/main/java/xyz/tcheeric/cashu/vault/db/CashuVaultApplication.java
+++ cashu-vault-jpa/src/main/java/xyz/tcheeric/cashu/vault/db/CashuVaultApplication.java
@@ -2,6 +2,7 @@ package xyz.tcheeric.cashu.vault.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import xyz.tcheeric.cashu.vault.db.client.VaultClient;
 import xyz.tcheeric.cashu.vault.db.model.KeyEntity;
@@ -12,27 +13,30 @@ import xyz.tcheeric.cashu.vault.db.model.ProofEntity;
 @SpringBootApplication
 public class CashuVaultApplication {
 
+    @Value("${vault.baseUrl:http://localhost:3333}")
+    private String baseUrl;
+
     public static void main(String[] args) {
 		SpringApplication.run(CashuVaultApplication.class, args);
 	}
 
 	@Bean
-	public VaultClient<MintEntity> vaultMintClient() {
-		return new VaultClient<>(MintEntity.class);
-	}
+        public VaultClient<MintEntity> vaultMintClient() {
+                return new VaultClient<>(MintEntity.class, baseUrl);
+        }
 
 	@Bean
-	public VaultClient<KeySetEntity> vaultKeySetClient() {
-		return new VaultClient<>(KeySetEntity.class);
-	}
+        public VaultClient<KeySetEntity> vaultKeySetClient() {
+                return new VaultClient<>(KeySetEntity.class, baseUrl);
+        }
 
 	@Bean
-	public VaultClient<ProofEntity> vaultProofClient() {
-		return new VaultClient<>(ProofEntity.class);
-	}
+        public VaultClient<ProofEntity> vaultProofClient() {
+                return new VaultClient<>(ProofEntity.class, baseUrl);
+        }
 
 	@Bean
-	public VaultClient<KeyEntity> vaultKeyClient() {
-		return new VaultClient<>(KeyEntity.class);
-	}
+        public VaultClient<KeyEntity> vaultKeyClient() {
+                return new VaultClient<>(KeyEntity.class, baseUrl);
+        }
 }
--- cashu-vault-jpa/src/main/java/xyz/tcheeric/cashu/vault/db/client/VaultClient.java
+++ cashu-vault-jpa/src/main/java/xyz/tcheeric/cashu/vault/db/client/VaultClient.java
@@ -5,11 +5,9 @@ import jakarta.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import xyz.tcheeric.cashu.vault.db.model.BaseEntity;
 
@@ -19,24 +17,38 @@ import java.util.List;
 public class VaultClient<T extends BaseEntity> {
     protected final RestTemplate restTemplate;
 
-    //@Value("${vault.baseUrl:http://localhost:8080}")
-    private String baseUrl = "http://localhost:3333";
+    private String baseUrl;
+    private static final String DEFAULT_BASE_URL = "http://localhost:3333";
 
     private final Class<T> entityType;
 
     private final String pathSegment;
 
     public VaultClient(Class<T> entityType) {
-        this(
-                entityType,
-                entityType.getAnnotation(Entity.class).name()
-        );
+        this(entityType, entityType.getAnnotation(Entity.class).name(), loadBaseUrl());
     }
 
-    private VaultClient(Class<T> entityType, String pathSegment) {
+    private VaultClient(Class<T> entityType, String pathSegment, String baseUrl) {
         this.restTemplate = new RestTemplate();
         this.entityType = entityType;
         this.pathSegment = pathSegment;
+        this.baseUrl = baseUrl;
+    }
+
+    public VaultClient(Class<T> entityType, String baseUrl) {
+        this(entityType, entityType.getAnnotation(Entity.class).name(), baseUrl);
+    }
+
+    private static String loadBaseUrl() {
+        String env = System.getenv("VAULT_BASE_URL");
+        if (env != null && !env.isBlank()) {
+            return env;
+        }
+        String property = System.getProperty("vault.baseUrl");
+        if (property != null && !property.isBlank()) {
+            return property;
+        }
+        return DEFAULT_BASE_URL;
     }
 
     public T store(T entity) {
