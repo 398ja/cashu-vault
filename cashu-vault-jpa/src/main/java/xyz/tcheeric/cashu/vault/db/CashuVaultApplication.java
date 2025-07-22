b29486060f91410735549658d309ed7b8f92b10d
erict875 <erict875@tuta.io>
2025-07-22 18:28:54 +0100

    Refactor CashuVaultApplication to improve readability and structure

--- cashu-vault-jpa/src/main/java/xyz/tcheeric/cashu/vault/db/CashuVaultApplication.java
+++ cashu-vault-jpa/src/main/java/xyz/tcheeric/cashu/vault/db/CashuVaultApplication.java
@@ -1,11 +1,11 @@
 package xyz.tcheeric.cashu.vault.db;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import xyz.tcheeric.cashu.vault.db.config.VaultBaseProperties;
 import xyz.tcheeric.cashu.vault.db.client.VaultClient;
+import xyz.tcheeric.cashu.vault.db.config.VaultBaseProperties;
 import xyz.tcheeric.cashu.vault.db.model.KeyEntity;
 import xyz.tcheeric.cashu.vault.db.model.KeySetEntity;
 import xyz.tcheeric.cashu.vault.db.model.MintEntity;
@@ -17,27 +17,31 @@ public class CashuVaultApplication {
 
     private final VaultBaseProperties vaultBaseProperties;
 
+    public CashuVaultApplication() {
+        this.vaultBaseProperties = new VaultBaseProperties();
+    }
+
     public static void main(String[] args) {
-		SpringApplication.run(CashuVaultApplication.class, args);
-	}
+        SpringApplication.run(CashuVaultApplication.class, args);
+    }
 
-	@Bean
-        public VaultClient<MintEntity> vaultMintClient() {
-                return new VaultClient<>(MintEntity.class, vaultBaseProperties.getUrl());
-        }
+    @Bean
+    public VaultClient<MintEntity> vaultMintClient() {
+        return new VaultClient<>(MintEntity.class, vaultBaseProperties.getUrl());
+    }
 
-	@Bean
-        public VaultClient<KeySetEntity> vaultKeySetClient() {
-                return new VaultClient<>(KeySetEntity.class, vaultBaseProperties.getUrl());
-        }
+    @Bean
+    public VaultClient<KeySetEntity> vaultKeySetClient() {
+        return new VaultClient<>(KeySetEntity.class, vaultBaseProperties.getUrl());
+    }
 
-	@Bean
-        public VaultClient<ProofEntity> vaultProofClient() {
-                return new VaultClient<>(ProofEntity.class, vaultBaseProperties.getUrl());
-        }
+    @Bean
+    public VaultClient<ProofEntity> vaultProofClient() {
+        return new VaultClient<>(ProofEntity.class, vaultBaseProperties.getUrl());
+    }
 
-	@Bean
-        public VaultClient<KeyEntity> vaultKeyClient() {
-                return new VaultClient<>(KeyEntity.class, vaultBaseProperties.getUrl());
-        }
+    @Bean
+    public VaultClient<KeyEntity> vaultKeyClient() {
+        return new VaultClient<>(KeyEntity.class, vaultBaseProperties.getUrl());
+    }
 }
